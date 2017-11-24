ccede5a26009a46d41a34dd7fcf03c02006bf304
Юрій Матійчик <jurmat.bbs@gmail.com>
2017-11-24 02:10:01 +0200

    first commit

--- app/js/src.js
+++ app/js/src.js
@@ -2,4 +2,17 @@ $(document).ready(function(){
 	$( "#select" ).niceSelect({
 	
 	});
+	var i =8
+$('.plus').click(function(){
+    i++
+  $('.res').html(i)
+});
+$('.min').click(function(){
+    i--
+  $('.res').html(i)
+  if(i<0){
+  i=8
+  $('.res').html(i)
+  }
+});
 });
